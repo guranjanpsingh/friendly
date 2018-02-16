965d890b62ff1d06b38249c99d67aa1fa6d8c2c8
Guranjan Singh <guranjansingh@Guranjans-MacBook-Pro.local>
2018-02-15 19:47:37 -0800

    first commit

---
+++ friendly/friendly_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FriendlyApiConfig(AppConfig):
+    name = 'friendly_api'
---
+++ friendly/friendly_api/models/users.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from .managers.usersManager import UserProfileManager
+
+
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
